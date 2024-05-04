d7e307817ce44ca818515d3de32102e83974b788
heathergibb <hrgibb@gmail.com>
2024-05-04 17:13:19 -0600

    added functionality to change h2 to menu item after clicked

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,6 @@
 const navMenu = document.querySelector(".menu-links");
 const navButton = document.querySelector("#menu");
+const title = document.querySelector("#title");
 
 navButton.addEventListener("click", () => {
     navMenu.classList.toggle("show");
@@ -10,5 +11,11 @@ window.addEventListener("resize", function() {
     if (this.window.innerWidth >= 768) {
         navMenu.classList.remove("show");
         navButton.classList.remove("show");
+    }
+})
+
+navMenu.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A') {
+        title.textContent = event.target.textContent;
     }
 })
